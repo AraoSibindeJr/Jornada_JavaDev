a2949cf5c4d7ba52cfe0f4def6ae74c97571d72b
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-03 09:42:16 +0200

    feat: project started

---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojomaratonajavaIntroducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println(" Ola Dev Dojo Academy! ");
+    }
+}
---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojomaratonajavaIntroducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        /*Tipos Primitivos: Existem oito (8) tipos primitivos em java:
+         - int; char; double; long; boolean; float; short; bite.
+         - Variavel: e um espaco criado para armazenar determinados tipos de dados na memoria
+         da maquina.
+        */
+        int num; // Criacao de uma variavel
+        num = 10; // Declaracao de uma variavel
+        boolean op = false; // Criacao e declaracao de variavel em uma so linha de codigo (recomendado)
+        System.out.println(num); // Saida de dados
+        System.out.println(" A resposata e: " + op ); // (+) Simbolo de concatenacao
+    }
+}
