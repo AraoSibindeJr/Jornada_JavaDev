d4e0881289c8b47ead52e0c7d584839632601b33
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-05 20:40:19 +0200

    feat: lesson 04 completed [Operadores]

--- Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula04Operadores.java
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula04Operadores.java
@@ -9,5 +9,27 @@ public class Aula04Operadores {
         System.out.println(n1-n2);
         System.out.println(n1/n2);
         System.out.println(n1*n2);
+        // Operadores Relacionais ( < ; > ; <= ; >= ; == ; != )
+        boolean op1 = 10 > 11; //false
+        boolean op2 = 10 < 11; //true
+        boolean op3 = 10 <= 11.9; //true
+        boolean op4 = 10 >= 112; //false
+        boolean op5 = 10 == 10; //true
+        boolean op6 = 10 != 0;//true
+        // Operadores Logicos ( AND (&&) ; OR (||) ; NOT(!)
+        int op7 = 200;
+        boolean testeLogico = (n1 + n1 > op7 && n2 - n1 == op7); // false
+        boolean testeLogico2 = (n1 + n1 < op7 || n2 - n1 == op7); // true
+        // Operadores de atribuicao && Operadores Unarios
+        int num = 10;
+        int num2 = 0;
+        num = num + 10; // Incremento normal
+        num += 10; // Usando Operador de atribuicao
+        num2++; // Usando operador unario
+
+        ++num; //Pre - Incremento
+        num++; //Pos - Incremento
+
+
     }
 }
