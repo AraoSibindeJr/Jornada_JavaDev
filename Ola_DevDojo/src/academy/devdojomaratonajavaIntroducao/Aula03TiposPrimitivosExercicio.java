b7e2c132492d5ace8b8ee54079bd48995c5f9433
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-05 09:30:25 +0200

    feat: Exercise 01 done

---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,22 @@
+package academy.devdojomaratonajavaIntroducao;
+/*     EXERCICIO
+*   Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem.
+* Eu, <nome>, morando no endereco <endereco>, confirmo que recebi o salario de <salario>, na
+* data <data>.
+*/
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        // Criacao e declaracao das variaveis
+        String nome = " Arao Sibinde";
+        int endereco = 202;
+        double salario = 750000;
+        String dataRecebimentoSalario = "15/07/2038";
+        // Duas formas de saida de informacao:
+        // 1 - Colocar toda mensagem numa var do tipo "string"
+        // 2 - Usar o "sout" directamente.
+        String mensagemFinal = "Eu,"+nome+ ", morando no endereco "+endereco+ ", confirmo que recebi o salario de "+salario+"$ na data "+dataRecebimentoSalario;
+        System.out.println(mensagemFinal); // Usando a forma "1"
+
+    }
+}
