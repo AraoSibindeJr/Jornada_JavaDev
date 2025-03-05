d3263f09ea102592bdbb4d2a912dd6739d372f9f
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-05 08:47:50 +0200

    feat: Lessons ]9;12] added

--- Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula02TiposPrimitivos.java
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula02TiposPrimitivos.java
@@ -12,5 +12,11 @@ public class Aula02TiposPrimitivos {
         boolean op = false; // Criacao e declaracao de variavel em uma so linha de codigo (recomendado)
         System.out.println(num); // Saida de dados
         System.out.println(" A resposata e: " + op ); // (+) Simbolo de concatenacao
+        // CASTING: Forca um valor a entrar numa variavel que em termos normais, nao suporta o valor.
+        // ESTRUTURA DO CASTING: [tipo] [nome] = (tipo casting) [valor] [inicial do tipo]
+        int n = (int) 100000000000000L; // Exemplo de casting
+        // STRING: A string nao e um tipo primitivo, e um tipo de referencia, isto e, e uma classe.
+        // A sua sintaxe inicia com a primeira letra maiuscula.
+        String nome = "Arao Jr"; //Exemplo de string.
     }
 }
