7c64c09fdf007a60c10b513c66b626ceba1879a4
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-05 10:11:44 +0200

    feat: lesson 04 added

---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojomaratonajavaIntroducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // Operadores Matematicos Basicos (Soma;Subtracao;Divisao;Multiplicacao)
+        int n1 = 10;
+        int n2 = 10;
+        System.out.println(n1+n2);
+        System.out.println(n1-n2);
+        System.out.println(n1/n2);
+        System.out.println(n1*n2);
+    }
+}
