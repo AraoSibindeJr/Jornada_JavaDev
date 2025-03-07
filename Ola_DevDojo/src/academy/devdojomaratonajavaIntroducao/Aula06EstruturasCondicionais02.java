1a5be4f4cbd19de8f6879578e39129c9f9bc9838
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-07 22:27:21 +0200

    feat: lesson added [Estruturas Condicionais Exercicios]

---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula06EstruturasCondicionais02.java
@@ -0,0 +1,28 @@
+package academy.devdojomaratonajavaIntroducao;
+
+public class Aula06EstruturasCondicionais02 {
+    public static void main(String[] args) {
+      /* Operador Ternario: E uma forma mais reduzida e simples de fazer condicoes, que
+       por vezes pode substituir a "If - Else".
+       */
+        // Estrutura: [var] = (condicao) ? {parte True} : {parte False};
+        int num = 10;
+        int num2 = 10;
+        String resp = (num + num2 == 20)? "SIM":"NAO";
+        System.out.println(resp);
+        /*Switch case: E uma estrutura de condicao geralmente usada quando temos que verificar muitas
+        * condicoes em forma de "Menu".*/
+        /*ESTRUTURA DO SWITCH CASE
+        * Switch(var){
+        *   case 1:
+        *       [Bloco de codigo]
+        *   break;
+        *   case 2:
+        *       [Bloco de codigo]
+        *   break;
+        *   default:
+        *       [Bloco de codigo]
+        *   breal;
+        * }*/
+    }
+}
---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula07EstruturasCondicionaisExercicios.java
@@ -0,0 +1,49 @@
+package academy.devdojomaratonajavaIntroducao;
+
+public class Aula07EstruturasCondicionaisExercicios {
+    public static void main(String[] args) {
+        /*EXERCICIO 01: Cria um codigo que, tendo em conta um numero [1 ate 7], mostra
+        * o dia da semana. Considere Domingo = 1
+        * */
+        int n = 4;
+        switch (n){
+            case 1:
+                System.out.println("DOMINGO");
+                break;
+            case 2:
+                System.out.println("SEGUNDA");
+                break;
+            case 3:
+                System.out.println("Terca");
+                break;
+            case 4:
+                System.out.println("QUARTA");
+                break;
+            case 5:
+                System.out.println("QUINTA");
+                break;
+            case 6:
+                System.out.println("SEXTA");
+                break;
+            case 7:
+                System.out.println("SABADO");
+                break;
+            default:
+                System.out.println("ERROR");
+                break;
+        }
+
+        /*EXERCICIO 02: Imprima o sexo, Masculino ou Feminino, tendo em conta "M" = Masculino e
+        * "F" = Feminino. NOTA: USANDO OPERADOR TERNARIO
+        * */
+        char m = 'M';
+        if(m == 'M'){
+            System.out.println(" Sexo = Masculino");
+        }else if( m == 'F'){
+            System.out.println(" Sexo = Feminino");
+        }else {
+            System.out.println("NOTA: Apenas {M/F}");
+        }
+
+    }
+}
