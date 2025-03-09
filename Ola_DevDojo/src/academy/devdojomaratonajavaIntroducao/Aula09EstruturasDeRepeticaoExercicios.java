52b0e0325087110df3ec57dc794e633f1c96d6d7
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-03-09 22:22:02 +0200

    feat: lesson added [Estruturas de Repeticao Exercicios]

---
+++ Ola_DevDojo/src/academy/devdojomaratonajavaIntroducao/Aula09EstruturasDeRepeticaoExercicios.java
@@ -0,0 +1,24 @@
+package academy.devdojomaratonajavaIntroducao;
+
+public class Aula09EstruturasDeRepeticaoExercicios {
+    public static void main(String[] args) {
+        /*
+        * EXERCICIO 01: Imprimir todos numeros pares de 1 ate 100
+        * EXERCICIO 02: Imprimir apenas os primeiro 25 numeros existentes no intervalo de 1 ate 100 (Usar Break
+        * */
+
+        // EXERCICIO 01
+        for (int i = 0; i <= 100; i++) {
+            if(i%2==0){//Condicao de paridade de um numero
+                System.out.println(i);
+            }
+        }
+        //EXERCICIO 02
+        for (int i = 0; i <= 100; i++) {
+            if(i > 25){
+                break;//Vai sair da estrutura de repeticao, quando a condicao for verdadeira
+            }
+            System.out.println(i);
+        }
+    }
+}
